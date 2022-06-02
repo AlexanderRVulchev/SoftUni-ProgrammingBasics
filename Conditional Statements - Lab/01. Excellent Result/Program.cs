72edfeefc2a717880f01db956b6cebad66fba40c
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-02 22:32:14 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/01. Excellent Result/Program.cs
@@ -0,0 +1,17 @@
+﻿//Първата задача от тази тема е да се напише конзолна програма, която чете оценка (дробно число),
+//въведена от потребителя и отпечатва "Excellent!", ако оценката е 5.50 или по-висока.
+
+using System;
+
+    internal class Program
+    {
+        static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
