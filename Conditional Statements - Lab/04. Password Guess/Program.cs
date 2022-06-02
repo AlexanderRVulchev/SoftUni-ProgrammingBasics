ce233d34f1e27d9671f9362222e736f60e26ebc0
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-02 22:42:50 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/04. Password Guess/Program.cs
@@ -0,0 +1,18 @@
+﻿//Да се напише програма, която чете парола (текст), въведена от потребителя и проверява дали въведената парола
+//съвпада с фразата "s3cr3t!P@ssw0rd". При съвпадение да се изведе "Welcome". При несъвпадение да се изведе "Wrong password!". 
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        string pass = Console.ReadLine();
+
+        if (pass == "s3cr3t!P@ssw0rd")
+        {
+            Console.WriteLine("Welcome");
+        }
+        else Console.WriteLine("Wrong password!");
+    }
+}
