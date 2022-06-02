e85206493574cf87570c3044d8286bd9570af9f3
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-02 22:43:33 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/06. Speed Info/Program.cs
@@ -0,0 +1,23 @@
+﻿//Да се напише програма, която чете скорост (реално число), въведена от потребителя и отпечатва информация за скоростта. 
+//•	При скорост до 10 (включително) отпечатайте "slow"
+//•	При скорост над 10 и до 50 (включително) отпечатайте "average"
+//•	При скорост над 50 и до 150 (включително) отпечатайте "fast"
+//•	При скорост над 150 и до 1000 (включително) отпечатайте "ultra fast"
+//•	При по-висока скорост отпечатайте "extremely fast"
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        double speed = double.Parse(Console.ReadLine());
+
+        if (speed <= 10) Console.WriteLine("slow");
+        else if (speed <= 50) Console.WriteLine("average");
+        else if (speed <= 150) Console.WriteLine("fast");
+        else if (speed <= 1000) Console.WriteLine("ultra fast");
+        else Console.WriteLine("extremely fast");
+
+    }
+}
