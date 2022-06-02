922e163c2a6780f8b175dee1af6ce06d62a33695
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-02 22:42:09 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/02. Greater Number/Program.cs
@@ -0,0 +1,16 @@
+﻿//Да се напише програма, която чете две цели числа въведени от потребителя и отпечатва по-голямото от двете. 
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+
+        if (num1 >= num2) Console.WriteLine(num1);
+        else Console.WriteLine(num2);
+
+    }
+}
