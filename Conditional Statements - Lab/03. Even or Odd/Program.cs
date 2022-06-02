3f2215531655ded33d87ce3b1b00a2f849bc8ec4
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-02 22:42:30 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/03. Even or Odd/Program.cs
@@ -0,0 +1,15 @@
+﻿//Да се напише програма, която чете цяло число, въведено от потребителя, и печата дали е четно или нечетно. 
+//Ако е четно отпечатайте "even", ако е нечетно "odd".
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0) Console.WriteLine("even");
+        else Console.WriteLine("odd");
+    }
+}
