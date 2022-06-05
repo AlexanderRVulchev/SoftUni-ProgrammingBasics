883d0521c184a0402a35743bd4a49f2d1059f450
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-05 10:38:50 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/03. Animal Type/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишете програма, която отпечатва класа на животното според неговото име, въведено от потребителя.
+//1.	dog -> mammal
+//2.	crocodile, tortoise, snake -> reptile
+//3.	others -> unknown
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string animal = Console.ReadLine();
+
+        switch (animal)
+        {
+            case "dog":
+                Console.WriteLine("mammal"); break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                Console.WriteLine("reptile"); break;
+            default: Console.WriteLine("unknown"); break;
+        }
+    }
+}
