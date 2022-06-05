2ca909a25c096409292278ce69fa78a71e59dbfb
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-05 10:40:54 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/10. Invalid Number/Program.cs
@@ -0,0 +1,16 @@
+﻿//Дадено число е валидно, ако е в диапазона [100…200] или е 0. Да се напише програма, която чете цяло число,
+//въведено от потребителя, и печата "invalid" ако въведеното число не е валидно. 
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int num = int.Parse(Console.ReadLine());
+
+		if (num >= 100 && num <= 200 || num == 0) ;
+		else Console.WriteLine("invalid");
+
+	}
+}
