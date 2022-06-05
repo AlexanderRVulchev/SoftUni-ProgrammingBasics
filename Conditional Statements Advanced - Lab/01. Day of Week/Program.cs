9c4bd34f8288af4623b259f064b2e1a64bedb8a7
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-05 10:24:32 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишете програма, която чете цяло число, въведено от потребителя, и отпечатва ден от седмицата (на английски език), в граници[1...7]
+//или отпечатва "Error" в случай, че въведеното число е невалидно. 
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int day = int.Parse(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1: Console.WriteLine("Monday"); break;
+            case 2: Console.WriteLine("Tuesday"); break;
+            case 3: Console.WriteLine("Wednesday"); break;
+            case 4: Console.WriteLine("Thursday"); break;
+            case 5: Console.WriteLine("Friday"); break;
+            case 6: Console.WriteLine("Saturday"); break;
+            case 7: Console.WriteLine("Sunday"); break;
+            default: Console.WriteLine("Error"); break;
+        }
+    }
+}
---
+++ Conditional Statements Advanced - Lab/02.Weekend or Working Day/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишете програма която, чете ден от седмицата (текст), на английски език - въведен от потребителя.
+//Ако денят е работен отпечатва на конзолата - "Working day", ако е почивен - "Weekend".
+//Ако се въведе текст различен от ден от седмицата да се отпечата - "Error".
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string day = Console.ReadLine();
+
+        switch (day)
+        {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                Console.WriteLine("Working day"); break;
+            case "Saturday":
+            case "Sunday":
+                Console.WriteLine("Weekend"); break;
+            default: Console.WriteLine("Error"); break;
+        }
+    }
+}
