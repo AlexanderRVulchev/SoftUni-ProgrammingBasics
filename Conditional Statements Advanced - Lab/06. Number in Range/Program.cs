33d3ebd7fb6d15b25f8cec77d07da082b068d270
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-05 10:39:46 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/06. Number in Range/Program.cs
@@ -0,0 +1,18 @@
+﻿//Да се напише програма, която проверява дали въведеното от потребителя число е в интервала [-100, 100] и е различно от 0
+//и извежда "Yes", ако отговаря на условията, или "No" ако е извън тях.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int number = int.Parse(Console.ReadLine());
+		bool inRange = false;
+
+		if (number >= -100 && number <= 100 && number != 0) inRange = true;
+
+		if (inRange) Console.WriteLine("Yes");
+		else Console.WriteLine("No");
+	}
+}
