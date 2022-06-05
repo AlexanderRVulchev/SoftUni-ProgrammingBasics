e67d487b305119d9acbfb5883ad5f1569c5ab06d
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-05 10:40:19 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/08.Cinema Ticket/Program.cs
@@ -0,0 +1,27 @@
+﻿//Да се напише програма която чете ден от седмицата (текст) – въведен от потребителя и принтира на конзолата цената на билет за кино според деня от седмицата:
+//Monday Tuesday	Wednesday	Thursday	Friday	Saturday	Sunday
+//12	12	14	14	12	16	16
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string day = Console.ReadLine();
+
+		switch (day)
+		{
+			case "Monday":
+			case "Tuesday":
+			case "Friday":
+				Console.WriteLine("12"); break;
+			case "Wednesday":
+			case "Thursday":
+				Console.WriteLine("14"); break;
+			case "Saturday":
+			case "Sunday":
+				Console.WriteLine("16"); break;
+		}
+	}
+}
