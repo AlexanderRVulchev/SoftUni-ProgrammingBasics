30d779784f1f5c1360efc642f4b69b59092436a0
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-05 10:39:12 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/04. Personal Titles/Program.cs
@@ -0,0 +1,27 @@
+﻿//Да се напише конзолна програма, която прочита възраст (реално число) и пол('m' или 'f'), въведени от потребителя, и отпечатва обръщение измежду следните:
+//•	"Mr." – мъж(пол 'm') на 16 или повече години
+//•	"Master" – момче (пол 'm') под 16 години
+//•	"Ms." – жена(пол 'f') на 16 или повече години
+//•	"Miss" – момиче (пол 'f') под 16 години
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		double age = double.Parse(Console.ReadLine());
+		string gender = Console.ReadLine();
+
+		if (gender == "m")
+		{
+			if (age >= 16) Console.WriteLine("Mr.");
+			else Console.WriteLine("Master");
+		}
+		if (gender == "f")
+		{
+			if (age >= 16) Console.WriteLine("Ms.");
+			else Console.WriteLine("Miss");
+		}
+	}
+}
