f806d12f30cd180966759577963c3e685259f669
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-08 18:33:31 +0300

    Add files via upload

---
+++ For Loop - Lab/01. Numbers from 1 to 100/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишете програма, която отпечатва числата от 1 до 100, всяко на нов ред.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		for (int i = 1; i <= 100; i++)
+		{
+			Console.WriteLine(i);
+		}
+	}
+}
---
+++ For Loop - Lab/02. Numbers N.1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишете програма, която чете цяло положително число n, въведено от потребителя и печата числата от n до 1 в обратен ред.
+//Въведеното число n, винаги ще бъде по-голямо от 1.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		for (int i = n; i >= 1; i--)
+		{
+			Console.WriteLine(i);
+		}
+	}
+}
