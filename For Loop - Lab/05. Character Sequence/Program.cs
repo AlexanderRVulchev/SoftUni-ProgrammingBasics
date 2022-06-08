d49366d5310b3c606d5164232233cfc2df75d271
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-08 18:53:49 +0300

    Add files via upload

---
+++ For Loop - Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишете програма, която чете текст (стринг), въведен от потребителя и печата всеки символ от текста на отделен ред.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string str = Console.ReadLine();
+
+		for (int i = 0; i < str.Length; i++)
+		{
+			char letter = str[i];
+			Console.WriteLine(letter);
+		}
+	}
+}
