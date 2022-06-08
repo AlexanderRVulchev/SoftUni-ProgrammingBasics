5f4470157ab787621ffc5bab579f40fbd2047683
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-08 18:55:57 +0300

    Add files via upload

---
+++ For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿//Да се напише програма, която чете 2*n-на брой цели числа, подадени от потребителя, и проверява дали сумата на първите n числа (лява сума) е равна на сумата на вторите n числа (дясна сума).
+//При равенство печата " Yes, sum = " + сумата; иначе печата " No, diff = " + разликата.
+//Разликата се изчислява като положително число (по абсолютна стойност). 
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		int left = 0;
+		int right = 0;
+		for (int i = 1; i <= n; i++) left += int.Parse(Console.ReadLine());
+		for (int i = 1; i <= n; i++) right += int.Parse(Console.ReadLine());
+		if (left == right) Console.WriteLine("Yes, sum = {0}", left);
+		else Console.WriteLine("No, diff = {0}", Math.Abs(left - right));
+	}
+}
