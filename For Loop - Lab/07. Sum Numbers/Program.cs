cd0819d9c9bbb5feab82180b503082ec18c17c92
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-08 18:55:22 +0300

    Add files via upload

---
+++ For Loop - Lab/07. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿//Да се напише програма, която чете n-на брой цели числа, въведени от потребителя и ги сумира.
+//•	От първия ред на входа се въвежда броят числа n.
+//•	От следващите n реда се въвежда по едно цяло число.
+//Програмата трябва да прочете числата, да ги сумира и да отпечата сумата им. 
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		int sum = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			sum += int.Parse(Console.ReadLine());
+		}
+		Console.WriteLine(sum);
+	}
+}
