114fae62ece79bdeefd377b55702fbaa4c5dbcb9
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-08 18:54:10 +0300

    Add files via upload

---
+++ For Loop - Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿//Да се напише програма, която чете текст (стринг), въведен от потребителя, и изчислява и отпечатва сумата от стойностите на гласните букви според таблицата по-долу:
+//буква     a	e	i	o	u
+//стойност	1	2	3	4	5
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string str = Console.ReadLine();
+		int score = 0;
+		for (int i = 0; i < str.Length; i++)
+		{
+			char letter = str[i];
+			switch (letter)
+			{
+				case 'a': score += 1; break;
+				case 'e': score += 2; break;
+				case 'i': score += 3; break;
+				case 'o': score += 4; break;
+				case 'u': score += 5; break;
+			}
+		}
+		Console.WriteLine(score);
+	}
+}
