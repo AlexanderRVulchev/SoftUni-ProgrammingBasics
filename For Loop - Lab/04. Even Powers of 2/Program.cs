9c21f6d34db791e2d09deba828a86293b2558ebe
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-08 18:53:25 +0300

    Add files via upload

---
+++ For Loop - Lab/04. Even Powers of 2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Да се напише програма, която чете число n, въведено от потребителя, и печата четните степени на 2 ≤ 2n: 20, 22, 24, 26, …, 2n. 
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		int num = 1;
+		for (int i = 0; i <= n; i += 2)
+		{
+			Console.WriteLine(num);
+			num = num * 2 * 2;
+		}
+	}
+}
