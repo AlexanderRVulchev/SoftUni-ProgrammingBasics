0d0c45da48c1da84a3adcfc3b8013aad0c626275
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-08 18:53:05 +0300

    Add files via upload

---
+++ For Loop - Lab/03. Numbers 1.N with Step 3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишете програма, която чете число n, въведено от потребителя и отпечатва числата от 1 до n през 3.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		for (int i = 1; i <= n; i += 3)
+		{
+			Console.WriteLine(i);
+		}
+	}
+}
