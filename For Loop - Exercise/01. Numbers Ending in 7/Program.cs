38e627e8de0edf9d4533319638b0590dcce849b0
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-10 18:21:46 +0300

    Add files via upload

---
+++ For Loop - Exercise/01. Numbers Ending in 7/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишете програма, която отпечатва числата в диапазона от 1 до 1000, които завършват на 7.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		for (int i = 7; i <= 1000; i += 10) Console.WriteLine(i);
+	}
+}
---
+++ For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,24 @@
+﻿//Да се напише програма, която чете n-на брой цели числа, въведени от потребителя,
+//и проверява дали сред тях съществува число, което е равно на сумата на всички останали. 
+//•	Ако има такъв елемент печата "Yes" и на нов ред "Sum = "  + неговата стойност
+//•	Ако няма такъв елемент печата "No" и на нов ред "Diff = " + разликата между най-големия елемент и сумата на останалите (по абсолютна стойност)
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		int max = int.MinValue;
+		int sum = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			int newNumber = int.Parse(Console.ReadLine());
+			sum += newNumber;
+			max = Math.Max(newNumber, max);
+		}
+		if (max == sum - max) Console.WriteLine("Yes\nSum = {0}", max);
+		else Console.WriteLine("No\nDiff = {0}", Math.Abs(max - (sum - max)));
+	}
+}
