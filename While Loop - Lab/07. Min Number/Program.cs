44491decb549cf7a000717f544c8096a592d2556
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-11 07:56:00 +0300

    Add files via upload

---
+++ While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишете програма, която до получаване на командата "Stop", чете цели числа, въведени от потребителя и намира най-малкото измежду тях.
+//Въвежда се по едно число на ред. 
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        int min = int.MaxValue;
+        string input;
+
+        while (true)
+        {
+            int num;
+            input = Console.ReadLine();
+            if (input == "Stop") break;
+            else
+            {
+                num = int.Parse(input);
+                min = Math.Min(min, num);
+            }
+        }
+        Console.WriteLine(min);
+    }
+}
