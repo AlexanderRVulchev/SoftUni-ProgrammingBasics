a273595f894dc2e358d085de9802bc2990bc1fa0
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-11 07:54:57 +0300

    Add files via upload

---
+++ While Loop - Lab/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишете програма, която чете число n, въведено от потребителя, и отпечатва всички числа ≤ n от редицата: 1, 3, 7, 15, 31, ….
+//Всяко следващо число се изчислява като умножим предишното с 2 и добавим 1.
+
+using System;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int num = 1;
+
+        while (num <= n)
+        {
+            Console.WriteLine(num);
+            num = num * 2 + 1;
+        }
+    }
+}
