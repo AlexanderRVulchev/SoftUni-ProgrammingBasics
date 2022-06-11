b7315127d60d7af032544239941dfa5934e45acf
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-11 07:54:41 +0300

    Add files via upload

---
+++ While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишете програма, която чете цяло число от конзолата и на всеки следващ ред цели числа, докато тяхната сума стане по-голяма или равна на първоначалното число.
+//След приключване да се отпечата сумата на въведените числа.
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        int limit = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        while (sum < limit)
+        {
+            int newNum = int.Parse(Console.ReadLine());
+            sum += newNum;
+        }
+        Console.WriteLine(sum);
+    }
+}
