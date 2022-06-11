bf0d4e7b6084e74938eca28c596acee3d0910e71
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-11 07:55:47 +0300

    Add files via upload

---
+++ While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишете програма, която до получаване на командата "Stop", чете цели числа, въведени от потребителя и намира най-голямото измежду тях.
+//Въвежда се по едно число на ред. 
+
+using System;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        int max = int.MinValue;
+        string input;
+
+        while (true)
+        {
+            int num;
+            input = Console.ReadLine();
+            if (input == "Stop") break;
+            else
+            {
+                num = int.Parse(input);
+                max = Math.Max(max, num);
+            }
+        }
+        Console.WriteLine(max);
+    }
+}
