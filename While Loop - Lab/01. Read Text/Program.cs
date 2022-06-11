6522565ff981aa40f7041ea3d4545f15ffe1f64c
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-11 07:47:26 +0300

    Add files via upload

---
+++ While Loop - Lab/01. Read Text/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишете програма, която чете текст от конзолата (стринг) и го принтира, докато не получи командата "Stop".
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string str = "";
+		while (true)
+		{
+			str = Console.ReadLine();
+			if (str == "Stop") break;
+			Console.WriteLine(str);
+		}
+	}
+}
---
+++ While Loop - Lab/02. Password/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишете програма, която първоначално прочита име и парола на потребителски профил. След това чете парола за вход.
+//•	при въвеждане на грешна парола: потребителя да се подкани да въведе нова парола.
+//•	при въвеждане на правилна парола: отпечатваме "Welcome {username}!".
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        string pass = Console.ReadLine();
+        string attempt = string.Empty;
+
+        while (attempt != pass)
+        {
+            attempt = Console.ReadLine();
+        }
+        Console.WriteLine($"Welcome {name}!");
+    }
+}
