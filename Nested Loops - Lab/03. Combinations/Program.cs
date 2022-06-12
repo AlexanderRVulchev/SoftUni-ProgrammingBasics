a72fd34c942f374b2e5071a3890aec17d7f70c92
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-12 19:44:00 +0300

    Add files via upload

---
+++ Nested Loops - Lab/03. Combinations/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишете програма, която изчислява колко решения в естествените числа (включително и нулата) има уравнението:
+//x1 + x2 + x3 = n
+//Числото n е цяло число и се въвежда от конзолата. 
+
+using System;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int valid = 0;
+
+        for (int x1 = 0; x1 <= n; x1++)
+        {
+            for (int x2 = 0; x2 <= n; x2++)
+            {
+                for (int x3 = 0; x3 <= n; x3++)
+                {
+                    if (x1 + x2 + x3 == n) valid++;
+                }
+
+            }
+        }
+        Console.WriteLine(valid);
+    }
+}
