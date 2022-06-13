a900c47d32bb1abecbf5390f9373c8f0be41bc0a
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-13 19:23:35 +0300

    Add files via upload

---
+++ Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишете програма, която чете цяло число n, въведено от потребителя, и отпечатва пирамида от числа 
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int count = 1;
+        bool ending = false;
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write($"{count++} ");
+                if (count > n)
+                {
+                    ending = true;
+                    break;
+                }
+            }
+            Console.WriteLine();
+            if (ending) break;
+        }
+    }
+}
---
+++ Nested Loops - Exercise/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,50 @@
+﻿//Напишете програма, която чете от конзолата две шестцифрени цели числа в диапазона от 100000 до 300000.
+//Винаги първото въведено число ще бъде по малко от второто.
+//На конзолата да се отпечатат на 1 ред разделени с интервал всички числа, които се намират между двете, прочетени от конзолата числа и отговарят на следното условие:
+//•	сумата от цифрите на четни и нечетни позиции да са равни. Ако няма числа, отговарящи на условието на конзолата не се извежда резултат. 
+
+using System;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        int firstNum = int.Parse(Console.ReadLine());
+        int secondNum = int.Parse(Console.ReadLine());
+
+        for (int i = firstNum; i <= secondNum; i++)
+        {
+            int totalNum = i;
+            int currentNum = 0;
+            int odd = 0;
+            int even = 0;
+
+            currentNum = (totalNum % 10) / 1;
+            odd += currentNum;
+            totalNum -= currentNum;
+
+            currentNum = (totalNum % 100) / 10;
+            even += currentNum;
+            totalNum -= currentNum;
+
+            currentNum = (totalNum % 1000) / 100;
+            odd += currentNum;
+            totalNum -= currentNum;
+
+            currentNum = (totalNum % 10000) / 1000;
+            even += currentNum;
+            totalNum -= currentNum;
+
+            currentNum = (totalNum % 100000) / 10000;
+            odd += currentNum;
+            totalNum -= currentNum;
+
+            currentNum = (totalNum % 1000000) / 100000;
+            even += currentNum;
+            totalNum -= currentNum;
+
+            if (even == odd) Console.Write($"{i} ");
+        }
+    }
+}
