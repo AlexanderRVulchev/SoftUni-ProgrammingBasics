1d79d13dede267cdbb4de4df26566ca5968133f1
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-14 23:02:19 +0300

    Add files via upload

---
+++ First Steps in Coding - More Exercises/08. Circle Area and Perimeter/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишете програма, която чете от конзолата число r и пресмята и отпечатва лицето и периметъра на кръг / окръжност с радиус r,
+//като форматирате изхода в следния вид: "<calculated area>"
+//"<calculated parameter>".Форматирайте изходните данни до втория знак след десетичната запетая.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		double r = double.Parse(Console.ReadLine());
+		Console.WriteLine("{0:f2}", r * r * Math.PI);
+		Console.WriteLine("{0:f2}", 2 * r * Math.PI);
+	}
+}
