ffaffb6a408a9cd75a543adf117fce7202443115
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-14 23:00:40 +0300

    Add files via upload

---
+++ First Steps in Coding - More Exercises/03. Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишете програма, която чете градуси по скалата на Целзий (°C) и ги преобразува до градуси по скалата на Фаренхайт (°F).
+//Потърсете в Интернет подходяща формула, с която да извършите изчисленията.
+//Форматирате изхода до втория знак след десетичната запетая. 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double celsius = double.Parse(Console.ReadLine());
+        double fahrenheit = celsius * 1.8 + 32;
+        Console.WriteLine($"{fahrenheit:f2}");
+    }
+}
