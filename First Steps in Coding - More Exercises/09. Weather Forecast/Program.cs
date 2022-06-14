3eb7747ef8a50f97b02b2a300c711976de61f454
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-14 23:02:38 +0300

    Add files via upload

---
+++ First Steps in Coding - More Exercises/09. Weather Forecast/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишете програма, която познава дали е топло или студено навън.
+//От конзолата се чете един ред – текст, който подсказва какво е времето.
+//При въвеждане на "sunny" трябва да се отпечата "It's warm outside!".
+//Във всички останали случаи трябва да се отпечата "It's cold outside!". 
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		string input = Console.ReadLine();
+		if (input == "sunny") Console.WriteLine("It's warm outside!");
+		else Console.WriteLine("It's cold outside!");
+	}
+}
