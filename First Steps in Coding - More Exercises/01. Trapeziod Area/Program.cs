c8c86aa398af3e672b337ce5849e018b19fe068c
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-14 22:53:18 +0300

    Add files via upload

---
+++ First Steps in Coding - More Exercises/01. Trapeziod Area/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишете програма, която чете от конзолата три дробни числа b1, b2 и h и пресмята лицето на трапец с основи b1 и b2 и височина h.
+//Формулата за лице на трапец е (b1 + b2) *h / 2.
+//Отговорът трябва да е форматиран до втората цифра след десетичния знак.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double b1 = double.Parse(Console.ReadLine());
+        double b2 = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+        double area = (b1 + b2) * h / 2;
+        Console.WriteLine($"{area:f2}");
+    }
+}
---
+++ First Steps in Coding - More Exercises/02. Triangle Area/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишете програма, която чете от конзолата страна и височина на триъгълник и пресмята неговото лице.
+//Използвайте формулата за лице на триъгълник: area = a * h / 2.
+//Форматирате изхода до втория знак след десетичната запетая.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+        double area = a * h / 2;
+        Console.WriteLine($"{area:f2}");
+    }
+}
