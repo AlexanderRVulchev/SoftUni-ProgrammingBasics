818e659f7f49a7ffd07c3fb922afda844be64774
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-20 21:47:30 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - More Exercises/09. Numbers from 1 to 10/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишете програма, която отпечатва числата от 1 до 10, по едно на ред.
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		for (int i = 1; i <= 10; i++)
+		{
+			Console.WriteLine(i);
+		}
+	}
+}
