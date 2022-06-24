64b744ba5645ec855515d8d111f2c9f7aee49dde
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-24 20:28:49 +0300

    Add files via upload

---
+++ While-Loop - More Exercises/01. Dishwasher/Program.cs
@@ -0,0 +1,44 @@
+﻿//Гошо работи в ресторант и отговаря за зареждането на съдомиялната накрая на деня.
+//Вашата задача е да напишете програма, която изчислява, дали дадено закупено количество бутилки от препарат за съдомиялна е достатъчно,
+//за да измие определено количество съдове. Знае се, че всяка бутилка съдържа 750 мл. препарат, за 1 чиния са нужни 5 мл., а за тенджера 15 мл.
+//Приемете, че на всяко трето зареждане със съдове, съдомиялната се пълни само с тенджери, а останалите пъти с чинии.
+//Докато не получите команда "End" ще продължите да получавате бройка съдове, които трябва да бъдат измити.
+//Вход
+//От конзолата се четат:
+//•	Брой бутилки от препарат, който ще бъде използван за миенето на чинии - цяло число в интервала [1…10]
+//На всеки следващ ред, до получаване на командата "End" или докато количеството препарат не се изчерпи,
+//брой съдове, които трябва да бъдат измити - цяло число в интервала [1…100]
+//Изход
+//В случай, че количеството препарат е било достатъчно за измиването на съдовете:
+//"Detergent was enough!"
+//"{брой чисти чинии} dishes and {брой чисти тенджери} pots were washed."
+//"Leftover detergent {количество останал препарат} ml."
+//В случай, че количеството препарат не е било достатъчно за измиването на съдовете:
+//"Not enough detergent, {количество не достигнал препарат} ml. more necessary!"
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		double detergent = double.Parse(Console.ReadLine()) * 750;
+		int dishes = 0;
+		int pots = 0;
+		int counter = 1;
+
+		string input = Console.ReadLine();
+		while (input != "End")
+		{
+			int num = int.Parse(input);
+			if (counter % 3 == 0) { pots += num; detergent -= num * 15; }
+			else { dishes += num; detergent -= num * 5; }
+			counter++;
+			if (detergent < 0) { Console.WriteLine("Not enough detergent, {0} ml. more necessary!", Math.Abs(detergent)); return; }
+			input = Console.ReadLine();
+		}
+		Console.WriteLine("Detergent was enough!");
+		Console.WriteLine("{0} dishes and {1} pots were washed.", dishes, pots);
+		Console.WriteLine("Leftover detergent {0} ml.", detergent);
+	}
+}
---
+++ While-Loop - More Exercises/02. Report System/Program.cs
@@ -0,0 +1,61 @@
+﻿//На благотворително събитие плащанията за закупените продукти винаги се редуват:
+//плащане в брой и плащане с карта. Установени са следните правила за заплащане:
+//•	Ако продуктът надвишава 100лв., за него не може да се плати в брой
+//•	Ако продуктът е на цена под 10лв., за него не може да се плати с кредитна карта
+//Програмата приключва или след като получим команда "End" или след като средствата бъдат събрани.
+//Вход
+//От конзолата се четат:
+//•	Сумата, която се очаква да бъде събрана от продажбите - цяло число в интервала [1 ... 10000]
+//На всеки следващ ред, до получаване на командата "End" или докато не се съберат нужните средства:
+//цените на предметите, които ще бъдат закупени - цяло число в интервала [1 ... 500]
+//Изход
+//На конзолата да се отпечата:
+//•	При успешна транзакция: "Product sold!"
+//•	При неуспешна транзакция: "Error in transaction!"
+//•	Ако сумата на всички закупени продукти надвиши или достигне очакваната сума,
+//програмата трябва да приключи и на конзолата да се изпишат два реда:
+//o "Average CS: {средно плащане в кеш на човек}"
+//o "Average CC: {средно плащане с карта на човек}"
+//              Плащанията трябва да бъдат форматирани до втората цифра след десетичния знак.
+//•	При получаване на команда "End", да се изпише един ред:
+//o "Failed to collect required money for charity."
+
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int target = int.Parse(Console.ReadLine());
+		string input = Console.ReadLine();
+		int cash = 0;
+		int card = 0;
+		int counter = 1;
+		int cashCount = 0;
+		int cardCount = 0;
+
+		while (input != "End")
+		{
+			int num = int.Parse(input);
+			if (counter % 2 == 0) // card transaction
+			{
+				if (num < 10) Console.WriteLine("Error in transaction!");
+				else { Console.WriteLine("Product sold!"); card += num; cardCount++; }
+			}
+			else  // cash transaction
+			{
+				if (num > 100) Console.WriteLine("Error in transaction!");
+				else { Console.WriteLine("Product sold!"); cash += num; cashCount++; }
+			}
+			if (cash + card >= target) // check for reached target
+			{
+				Console.WriteLine("Average CS: {0:f2}", (double)cash / (double)cashCount);
+				Console.WriteLine("Average CC: {0:f2}", (double)card / (double)cardCount);
+				return;
+			}
+			counter++;
+			input = Console.ReadLine();
+		}
+		Console.WriteLine("Failed to collect required money for charity.");
+	}
+}
