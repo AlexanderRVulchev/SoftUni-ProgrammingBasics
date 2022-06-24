a3cff3837c5d3c048a19f899a11bfa346cde29b7
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-24 20:32:40 +0300

    Add files via upload

---
+++ While-Loop - More Exercises/05. Average Number/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишете програма, която прочита едно число n, след това прочита n на брой цели числа
+//и принтира средно аритметичното на тяхната сума число, форматирано до втората цифра след десетични знак.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("{0:f2}", (double)sum / n);
+    }
+}
