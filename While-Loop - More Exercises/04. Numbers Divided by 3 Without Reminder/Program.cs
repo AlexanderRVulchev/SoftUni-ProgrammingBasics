34277b0fa76a7aac8ccdf81d19aaae88e26d73f3
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-24 20:32:18 +0300

    Add files via upload

---
+++ While-Loop - More Exercises/04. Numbers Divided by 3 Without Reminder/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишете програма, която печата всички числа в интервала от 1 до 100, който се делят на 3 без остатък, по едно на ред.
+
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 3; i < 100; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
