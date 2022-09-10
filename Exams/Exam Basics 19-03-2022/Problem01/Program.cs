9c3b30c20e9afd6ebf09193bb6042104565c7ba6
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-10 08:35:46 +0300

    Add files via upload

---
+++ Exams/Exam Basics 19-03-2022/Problem01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem01
+{
+    internal class Program
+    {
+        static void Main()
+        {            
+            double processor = double.Parse(Console.ReadLine()) * 1.57;
+            double videoCard = double.Parse(Console.ReadLine()) * 1.57;
+            double ram = double.Parse(Console.ReadLine()) * 1.57;
+            int ramAmount = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            processor -= processor * discount;
+            videoCard -= videoCard * discount;
+            ram *= ramAmount;
+            double total = processor + videoCard + ram;
+
+            Console.WriteLine($"Money needed - {total:f2} leva.");
+        }
+    }
+}
