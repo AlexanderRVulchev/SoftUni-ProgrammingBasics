691229c2f236f3020215d0f0eb7b5312c570831d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-11 08:43:07 +0300

    Add files via upload

---
+++ Exams/Exam Basics 19-03-2022/Problem05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Problem05
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int sea = int.Parse(Console.ReadLine());
+            int mountain = int.Parse(Console.ReadLine());
+            int profit = 0;
+
+            string input = Console.ReadLine();
+            while (input != "Stop")
+            {
+                if (input == "sea" && sea > 0) { sea--; profit += 680; }
+                if (input == "mountain" && mountain > 0) { mountain--; profit += 499; }
+                if (sea == 0 && mountain == 0) { Console.WriteLine("Good job! Everything is sold."); break; }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Profit: {profit} leva.");
+        }
+    }
+}
