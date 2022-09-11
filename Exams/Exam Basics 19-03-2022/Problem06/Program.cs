c7d8fd3600e53c91d3ccc8d67679c91003ffcd1c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-09-11 08:43:33 +0300

    Add files via upload

---
+++ Exams/Exam Basics 19-03-2022/Problem06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Problem06
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int a = 1; a <= 9; a++)
+                for (int b = 9; b >= a; b--)
+                    for (int c = 0; c <= 9; c++)
+                        for (int d = 9; d >= c; d--)
+                        {
+                             if (a + b + c + d == a * b * c * d && n % 10 == 5)
+                            {
+                                Console.WriteLine($"{a}{b}{c}{d}");
+                                return;
+                            }
+                            if ((a * b * c * d) / (a + b + c + d) == 3 && n % 3 == 0)
+                            {
+                                Console.WriteLine($"{d}{c}{b}{a}");
+                                return;
+                            }
+                        }
+            Console.WriteLine("Nothing found");
+        }
+    }
+}
